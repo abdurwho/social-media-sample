050aff487fd563ef671ad3830fed8bf14a1e379f
Abdur Rahman <66549091+abdurwho@users.noreply.github.com>
2023-04-11 12:22:40 +0530

    Update upload-edit-script.js

--- upload-edit-script.js
+++ upload-edit-script.js
@@ -104,34 +104,34 @@ const resetFilter = () => {
 
 // This is for downloading the image
 
-// const saveImage = () => {
-//   const canvas = document.createElement("canvas");
-//   const ctx = canvas.getContext("2d");
-//   canvas.width = previewImg.naturalWidth;
-//   canvas.height = previewImg.naturalHeight;
+const saveImage = () => {
+  const canvas = document.createElement("canvas");
+  const ctx = canvas.getContext("2d");
+  canvas.width = previewImg.naturalWidth;
+  canvas.height = previewImg.naturalHeight;
 
-//   ctx.filter = `brightness(${brightness}%) saturate(${saturation}%) invert(${inversion}%) grayscale(${grayscale}%)`;
-//   ctx.translate(canvas.width / 2, canvas.height / 2);
-//   if (rotate !== 0) {
-//     ctx.rotate((rotate * Math.PI) / 180);
-//   }
-//   ctx.scale(flipHorizontal, flipVertical);
-//   ctx.drawImage(
-//     previewImg,
-//     -canvas.width / 2,
-//     -canvas.height / 2,
-//     canvas.width,
-//     canvas.height
-//   );
+  ctx.filter = `brightness(${brightness}%) saturate(${saturation}%) invert(${inversion}%) grayscale(${grayscale}%)`;
+  ctx.translate(canvas.width / 2, canvas.height / 2);
+  if (rotate !== 0) {
+    ctx.rotate((rotate * Math.PI) / 180);
+  }
+  ctx.scale(flipHorizontal, flipVertical);
+  ctx.drawImage(
+    previewImg,
+    -canvas.width / 2,
+    -canvas.height / 2,
+    canvas.width,
+    canvas.height
+  );
 
-//   const link = document.createElement("a");
-//   link.download = "image.jpg";
-//   link.href = canvas.toDataURL();
-//   link.click();
-// };
+  const link = document.createElement("a");
+  link.download = "image.jpg";
+  link.href = canvas.toDataURL();
+  link.click();
+};
 
 filterSlider.addEventListener("input", updateFilter);
 resetFilterBtn.addEventListener("click", resetFilter);
-// saveImgBtn.addEventListener("click", saveImage);
+saveImgBtn.addEventListener("click", saveImage);
 fileInput.addEventListener("change", loadImage);
 chooseImgBtn.addEventListener("click", () => fileInput.click());
